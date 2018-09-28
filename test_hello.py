2a3e25cf23c012766f29b98354ea1d613e55e61f
Chris Wood <chris.wood@cloudreach.com>
2018-09-28 11:38:22 +0100

    Fix test name

--- test_hello.py
+++ test_hello.py
@@ -2,7 +2,7 @@ import unittest
 from hello import say_hello
 
 class test_hello(unittest.TestCase):
-    def test_no_env_var(self):
+    def test_hello(self):
         self.assertEqual(say_hello('world'), 'Hello world')
 
 if __name__ == '__main__':
